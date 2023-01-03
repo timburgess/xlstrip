cfaef5267fc4e20ff32c44fc633aaa0a0997f649
Tim Burgess <tim.burgess@lanzatech.com>
2023-01-04 12:54:36 +1300

    Update test spreadsheet

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,8 @@ const c = @cImport({
 });
 
 const std = @import("std");
+const debug = std.debug;
+const ArrayList = std.ArrayList;
 
 // enums for NodeType - https://www.gnu.org/software/dotgnu/pnetlib-doc/System/Xml/XmlNodeType.html
 const NodeType = enum(c_int) {
@@ -35,24 +37,24 @@ const parseOptions = c.XML_PARSE_NOBLANKS | c.XML_PARSE_NOCDATA | c.XML_PARSE_NO
 // print the node type
 fn printNodeTypeName(nodeType: NodeType) void {
     switch (nodeType) {
-        NodeType.None => std.debug.print("NodeType.None\n", .{}),
-        NodeType.Element => std.debug.print("NodeType.Element\n", .{}),
-        NodeType.Attribute => std.debug.print("NodeType.Attribute\n", .{}),
-        NodeType.Text => std.debug.print("NodeType.Text\n", .{}),
-        NodeType.CDATAField => std.debug.print("NodeType.CDATA\n", .{}),
-        NodeType.EntityRef => std.debug.print("NodeType.EntityRef\n", .{}),
-        NodeType.Entity => std.debug.print("NodeType.Entity\n", .{}),
-        NodeType.ProcessingInstruction => std.debug.print("NodeType.PI\n", .{}),
-        NodeType.Comment => std.debug.print("NodeType.Comment\n", .{}),
-        NodeType.Document => std.debug.print("NodeType.Document\n", .{}),
-        NodeType.DocumentType => std.debug.print("NodeType.DocumentType\n", .{}),
-        NodeType.DocumentFragment => std.debug.print("NodeType.DocumentFragment\n", .{}),
-        NodeType.Notation => std.debug.print("NodeType.Notation\n", .{}),
-        NodeType.Whitespace => std.debug.print("NodeType.Whitespace\n", .{}),
-        NodeType.SignificantWhitespace => std.debug.print("NodeType.SignificantWhitespace\n", .{}),
-        NodeType.EndElement => std.debug.print("NodeType.EndElement\n", .{}),
-        NodeType.EndEntity => std.debug.print("NodeType.EndEntity\n", .{}),
-        NodeType.XMLDeclaration => std.debug.print("NodeType.XMLDeclaration\n", .{}),
+        NodeType.None => debug.print("NodeType.None\n", .{}),
+        NodeType.Element => debug.print("NodeType.Element\n", .{}),
+        NodeType.Attribute => debug.print("NodeType.Attribute\n", .{}),
+        NodeType.Text => debug.print("NodeType.Text\n", .{}),
+        NodeType.CDATAField => debug.print("NodeType.CDATA\n", .{}),
+        NodeType.EntityRef => debug.print("NodeType.EntityRef\n", .{}),
+        NodeType.Entity => debug.print("NodeType.Entity\n", .{}),
+        NodeType.ProcessingInstruction => debug.print("NodeType.PI\n", .{}),
+        NodeType.Comment => debug.print("NodeType.Comment\n", .{}),
+        NodeType.Document => debug.print("NodeType.Document\n", .{}),
+        NodeType.DocumentType => debug.print("NodeType.DocumentType\n", .{}),
+        NodeType.DocumentFragment => debug.print("NodeType.DocumentFragment\n", .{}),
+        NodeType.Notation => debug.print("NodeType.Notation\n", .{}),
+        NodeType.Whitespace => debug.print("NodeType.Whitespace\n", .{}),
+        NodeType.SignificantWhitespace => debug.print("NodeType.SignificantWhitespace\n", .{}),
+        NodeType.EndElement => debug.print("NodeType.EndElement\n", .{}),
+        NodeType.EndEntity => debug.print("NodeType.EndEntity\n", .{}),
+        NodeType.XMLDeclaration => debug.print("NodeType.XMLDeclaration\n", .{}),
     }
 }
 
@@ -60,20 +62,78 @@ fn processNode(reader: ?*c.xmlTextReader) !void {
     const name = c.xmlTextReaderConstName(reader);
     const value = c.xmlTextReaderConstValue(reader);
     if (name != null) {
-        std.debug.print("name: {s}\n", .{name});
+        debug.print("name: {s}\n", .{name});
     } else {
-        std.debug.print("name is null\n", .{});
+        debug.print("name is null\n", .{});
     }
     if (value != null) {
-        std.debug.print("value: {s}\n", .{value});
+        debug.print("value: {s}\n", .{value});
     } else {
-        std.debug.print("value is null\n", .{});
+        debug.print("value is null\n", .{});
     }
     const nodeType = c.xmlTextReaderNodeType(reader);
     // convert to NodeType enum
     printNodeTypeName(@intToEnum(NodeType, nodeType));
 
-    std.debug.print("\n", .{});
+    debug.print("\n", .{});
+}
+
+// parse the xml and return an array of all text values of <t> elements as strings
+// fn readSharedStrings(buf: []const u8) ![][]const u8 {
+fn readSharedStrings(buf: []const u8) !void {
+    const reader = c.xmlReaderForMemory(buf.ptr, @intCast(c_int, buf.len), null, null, 0);
+    defer c.xmlFreeTextReader(reader);
+    // check reader != null
+    if (reader == null) {
+        debug.print("reader is null\n", .{});
+        return error.ReaderIsNull;
+    }
+
+    // read frootNode identify how many unqiue strings
+    var ret = c.xmlTextReaderRead(reader);
+    if (ret != 1) {
+        debug.print("ret != 1\n", .{});
+        return error.ReadFailed;
+    }
+
+    // get the uniqueCount attribute
+    const uniqueCount = c.xmlTextReaderGetAttribute(reader, "uniqueCount");
+    const uniqueCountInt = try std.fmt.parseInt(u32, std.mem.span(uniqueCount), 10);
+
+    // allocate an array of slices
+    var sharedStrings = try std.heap.c_allocator.alloc([]const u8, uniqueCountInt);
+
+    // read the remaining nodes
+    ret = c.xmlTextReaderRead(reader);
+    while (ret == 1) {
+        const nodeName = c.xmlTextReaderConstName(reader);
+        // if node is <t> then read the next node (text node)
+        if (std.mem.eql(u8, std.mem.span(nodeName), "t")) {
+            // read the next text node
+            ret = c.xmlTextReaderRead(reader);
+            if (ret == 1) {
+                const nodeType = c.xmlTextReaderNodeType(reader);
+                if (@intToEnum(NodeType, nodeType) == NodeType.Text) {
+                    const value = c.xmlTextReaderConstValue(reader);
+                    if (value != null) {
+                        debug.print("text value: {s}\n", .{std.mem.span(value)});
+                    }
+                }
+            }
+        }
+        ret = c.xmlTextReaderRead(reader);
+    }
+
+    // convert stringList to [][]const u8 and return
+    // var sharedStrings = try std.heap.c_allocator.alloc([]const u8, stringList.items.len);
+    // iterate over stringList and copy each item to sharedStrings
+    // var i: usize = 0;
+    // while (i < stringList.items.len) : (i += 1) {
+    //     sharedStrings[i] = try std.heap.c_allocator.dupe(u8, stringList.items[i]);
+    // }
+
+    _ = sharedStrings;
+    // return null;
 }
 
 // accepts a path to the zip file and returns a buffer containing the contents of internal file
@@ -81,32 +141,32 @@ fn readZipFileContents(path: [*c]const u8, filename: [*c]const u8) ![]u8 {
     const archive = c.zip_open(path, c.ZIP_RDONLY, null);
     defer _ = c.zip_close(archive);
     if (archive == null) {
-        std.debug.print("error: Failed to open zip file\n", .{});
+        debug.print("error: Failed to open zip file\n", .{});
         return error.FailedToOpenZipFile;
     }
 
-    var i: u64 = 0;
     var stat: c.zip_stat_t = undefined;
-    while (true) {
-        const ret = c.zip_stat_index(archive, i, 0, &stat);
-        if (ret != 0) {
-            break;
-        }
-        std.debug.print("name: {s}\n", .{stat.name});
-        i += 1;
-    }
+    // var i: u64 = 0;
+    // while (true) {
+    //     const ret = c.zip_stat_index(archive, i, 0, &stat);
+    //     if (ret != 0) {
+    //         break;
+    //     }
+    //     debug.print("name: {s}\n", .{stat.name});
+    //     i += 1;
+    // }
 
     // open filename and stat size
     const ret = c.zip_stat(archive, filename, 0, &stat);
     if (ret != 0) {
-        std.debug.print("error: Failed to open {s}\n", .{filename});
+        debug.print("error: Failed to open {s}\n", .{filename});
         return error.FailedToOpenSharedStrings;
     }
-    std.debug.print("size is {d} chars\n", .{stat.size});
+
     const strs = c.zip_fopen(archive, filename, 0);
     defer _ = c.zip_fclose(strs);
     if (strs == null) {
-        std.debug.print("error: Failed to open {s}\n", .{filename});
+        debug.print("error: Failed to open {s}\n", .{filename});
         return error.FailedToOpenSharedStrings;
     }
 
@@ -114,7 +174,7 @@ fn readZipFileContents(path: [*c]const u8, filename: [*c]const u8) ![]u8 {
     var buf = try std.heap.c_allocator.alloc(u8, stat.size);
     const read = c.zip_fread(strs, buf.ptr, stat.size);
     if (read != stat.size) {
-        std.debug.print("error: Failed to read {s}\n", .{filename});
+        debug.print("error: Failed to read {s}\n", .{filename});
         return error.FailedToReadSharedStrings;
     }
     return buf;
@@ -133,39 +193,29 @@ pub fn main() !void {
     // try bw.flush(); // don't forget to flush!
 
     const sharedStringsBuf = try readZipFileContents("src/test/spreadsheet1/Test_Tags_Spreadsheet.xlsx", "xl/sharedStrings.xml");
-    std.debug.print("{s}\n", .{sharedStringsBuf[0..]});
+    debug.print("{s}\n", .{sharedStringsBuf[0..]});
 
-    const reader = c.xmlReaderForMemory(sharedStringsBuf.ptr, @intCast(c_int, sharedStringsBuf.len), null, null, 0);
-    defer c.xmlFreeTextReader(reader);
-    // check reader != null
-    if (reader == null) {
-        std.debug.print("reader is null\n", .{});
-        return;
-    }
-
-    var ret = c.xmlTextReaderRead(reader);
-    while (ret == 1) {
-        processNode(reader) catch |err| {
-            std.debug.print("error: {s}\n", .{err});
-        };
-        ret = c.xmlTextReaderRead(reader);
-    }
+    try readSharedStrings(sharedStringsBuf);
+    // print the first 10 strings in arr
+    // for (arr[0..10]) |str| {
+    //     debug.print("{s}\n", .{str});
+    // }
 
     // const worksheetBuf = try readZipFileContents("src/test/spreadsheet1/Test_Tags_Spreadsheet.xlsx", "xl/worksheets/sheet1.xml");
-    // std.debug.print("{s}\n", .{worksheetBuf[0..]});
+    // debug.print("{s}\n", .{worksheetBuf[0..]});
 
     // const reader = c.xmlReaderForMemory(worksheetBuf.ptr, @intCast(c_int, worksheetBuf.len), null, null, 0);
     // defer c.xmlFreeTextReader(reader);
     // // check reader != null
     // if (reader == null) {
-    //     std.debug.print("reader is null\n", .{});
+    //     debug.print("reader is null\n", .{});
     //     return;
     // }
 
     // var ret = c.xmlTextReaderRead(reader);
     // while (ret == 1) {
     //     processNode(reader) catch |err| {
-    //         std.debug.print("error: {s}\n", .{err});
+    //         debug.print("error: {s}\n", .{err});
     //     };
     //     ret = c.xmlTextReaderRead(reader);
     // }
@@ -178,7 +228,7 @@ test "xml read" {
     defer c.xmlFreeParserCtxt(ctx);
     // assert context valid (not null)
     if (ctx == null) {
-        std.debug.print("context is null\n", .{});
+        debug.print("context is null\n", .{});
         return;
     }
 
@@ -186,7 +236,7 @@ test "xml read" {
     defer c.xmlFreeTextReader(reader);
     // check reader != null
     if (reader == null) {
-        std.debug.print("reader is null\n", .{});
+        debug.print("reader is null\n", .{});
         return;
     }
 
@@ -195,10 +245,10 @@ test "xml read" {
         const name = c.xmlTextReaderConstName(reader);
         const value = c.xmlTextReaderConstValue(reader);
         if (name != null) {
-            std.debug.print("name: {s}\n", .{name});
+            debug.print("name: {s}\n", .{name});
         }
         if (value != null) {
-            std.debug.print("value: {s}\n", .{value});
+            debug.print("value: {s}\n", .{value});
         }
         ret = c.xmlTextReaderRead(reader);
     }
